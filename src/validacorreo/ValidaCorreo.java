dc1c0826fe18ffb53ede8ac5c19c726f62605157
uber <uber@192.168.56.1>
2020-06-14 18:03:17 -0500

    Subiedo mi aporte1

---
+++ src/validacorreo/ValidaCorreo.java
@@ -0,0 +1,14 @@
+
+package validacorreo;
+
+
+public class ValidaCorreo {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Form1 f1 = new Form1();
+        f1.setVisible(true);
+    }
+    
+}
